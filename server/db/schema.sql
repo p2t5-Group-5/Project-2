eec1e74bb5fad4d90caa5a07d3e192b57fdd6fc4
Antonina S <antoninad22@gmail.com>
2025-03-31 21:48:28 -0500

    added usertype, categories and products tables to the schema

--- server/db/schema.sql
+++ server/db/schema.sql
@@ -4,31 +4,30 @@ DROP DATABASE IF EXISTS specialShop_db
 -- CREATE DATABASE
 CREATE DATABASE friends_db;
 
+CREATE TYPE usertype AS ENUM ('admin', 'buyer', 'seller');
 
-Create Table users (
-    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+CREATE TABLE users (
+    id SERIAL PRIMARY KEY,
     username VARCHAR(255) NOT NULL,
-    usertype ENUM('admin', 'buyer', 'seller') NOT NULL,
+    usertype usertype NOT NULL,
     password VARCHAR(255) NOT NULL,
-    email VARCHAR(255) NOT NULL,
-)
+    email VARCHAR(255) NOT NULL
+);
 
--- Create Table categories (
---     id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
---     name VARCHAR(50) NOT NULL,
---     description TEXT NOT NULL,
---     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
--- )
 
--- Create Table products (
---     id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
---     name VARCHAR(100) NOT NULL,
---     description TEXT NOT NULL,
---     category_id INT NOT NULL,
---     price DECIMAL(10, 2) NOT NULL,
---     stock INT NOT NULL,
---     seller_id INT NOT NULL,
---     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
---     FOREIGN KEY (seller_id) REFERENCES users(id),
---     FOREIGN KEY (category_id) REFERENCES categories(id)
--- )
+CREATE TABLE categories (
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(50) NOT NULL,
+    description TEXT NOT NULL
+);
+
+CREATE TABLE products (
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(100) NOT NULL,
+    description TEXT NOT NULL,
+    category_id INT NOT NULL,
+    price DECIMAL(10, 2) NOT NULL,
+    seller_id INT NOT NULL,
+    FOREIGN KEY (seller_id) REFERENCES users(id),
+    FOREIGN KEY (category_id) REFERENCES categories(id)
+);
