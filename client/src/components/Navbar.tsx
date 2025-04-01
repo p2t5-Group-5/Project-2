8803ff37785af521c15428e6892a924d3e81c7d9
Nukedeggz <sethmegg@yahoo.com>
2025-03-31 22:57:12 -0400

    add files for componters and pages. add some components to app

---
+++ client/src/components/Navbar.tsx
@@ -0,0 +1,37 @@
+import { useState, useEffect } from 'react';
+import { Link } from 'react-router-dom';
+import auth from '../utils/auth';
+
+const Navbar = () => {
+  // State to track the login status
+  const [loginCheck, setLoginCheck] = useState(false);
+
+  // Function to check if the user is logged in using auth.loggedIn() method
+  const checkLogin = () => {
+    if (auth.loggedIn()) {
+      setLoginCheck(true);  // Set loginCheck to true if user is logged in
+    }
+  };
+
+  // useEffect hook to run checkLogin() on component mount and when loginCheck state changes
+  useEffect(() => {
+    checkLogin();  // Call checkLogin() function to update loginCheck state
+  }, [loginCheck]);  // Dependency array ensures useEffect runs when loginCheck changes
+
+  return (
+    <div className=" ">
+        <button className=''>
+           <Link to='/'>Home</Link>
+        </button>
+        <button className=''>
+            <Link to='/shop'>Shop</Link>
+        </button>
+     
+      <button className=''>
+        <Link to='/Seller'>Seller</Link>
+      </button>
+    </div>
+  )
+}
+
+export default Navbar;
---
+++ client/src/pages/Shop.tsx
@@ -0,0 +1,7 @@
+import { useState, useEffect, useLayoutEffect } from "react";
+import { retrieveUsers } from "../api/userAPI";
+import type { UserData } from "../interfaces/UserData";
+
+
+
+export default Shop;
