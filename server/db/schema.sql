ce016060aba1814eed7487eb281d095a489f559b
Antonina S <antoninad22@gmail.com>
2025-03-31 21:15:31 -0500

    added files from activity 25

--- server/db/schema.sql
+++ server/db/schema.sql
@@ -2,35 +2,33 @@
 -- DROP DATABASE
 DROP DATABASE IF EXISTS specialShop_db
 -- CREATE DATABASE
-CREATE DATABASE specialShop_db;
+CREATE DATABASE friends_db;
 
 
 Create Table users (
     id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    username VARCHAR(50) NOT NULL,
-    email VARCHAR(100) NOT NULL,
-    password VARCHAR(50) NOT NULL,
+    username VARCHAR(255) NOT NULL,
     usertype ENUM('admin', 'buyer', 'seller') NOT NULL,
-    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+    password VARCHAR(255) NOT NULL,
+    email VARCHAR(255) NOT NULL,
 )
 
-Create Table categories (
-    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    name VARCHAR(50) NOT NULL,
-    description TEXT NOT NULL,
-    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
-)
+-- Create Table categories (
+--     id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+--     name VARCHAR(50) NOT NULL,
+--     description TEXT NOT NULL,
+--     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+-- )
 
-Create Table products (
-    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    name VARCHAR(100) NOT NULL,
-    description TEXT NOT NULL,
-    category_id INT NOT NULL,
-    price DECIMAL(10, 2) NOT NULL,
-    stock INT NOT NULL,
-    seller_id INT NOT NULL,
-    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    FOREIGN KEY (seller_id) REFERENCES users(id),
-    FOREIGN KEY (category_id) REFERENCES categories(id)
-)
-
+-- Create Table products (
+--     id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+--     name VARCHAR(100) NOT NULL,
+--     description TEXT NOT NULL,
+--     category_id INT NOT NULL,
+--     price DECIMAL(10, 2) NOT NULL,
+--     stock INT NOT NULL,
+--     seller_id INT NOT NULL,
+--     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+--     FOREIGN KEY (seller_id) REFERENCES users(id),
+--     FOREIGN KEY (category_id) REFERENCES categories(id)
+-- )
