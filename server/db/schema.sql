23c5d8ec62290f446b58dea3d50b0e5dd42c6c03
Jonathan Hummer <jonathan.hummer@gmail.com>
2025-03-31 20:48:07 -0400

    Database changes

--- server/db/schema.sql
+++ server/db/schema.sql
@@ -1,5 +1,36 @@
+-- SQLBook: Code
 -- DROP DATABASE
-DROP DATABASE IF EXISTS friends_db;
-
+DROP DATABASE IF EXISTS specialshop_db
 -- CREATE DATABASE
-CREATE DATABASE friends_db;
+CREATE DATABASE specialshop_db;
+
+
+Create Table users (
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    username VARCHAR(50) NOT NULL,
+    usertype ENUM('admin', 'buyer', 'seller') NOT NULL,
+    password VARCHAR(50) NOT NULL,
+    email VARCHAR(100) NOT NULL,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+)
+
+Create Table categories (
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(50) NOT NULL,
+    description TEXT NOT NULL,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+)
+
+Create Table products (
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(100) NOT NULL,
+    description TEXT NOT NULL,
+    category_id INT NOT NULL,
+    price DECIMAL(10, 2) NOT NULL,
+    stock INT NOT NULL,
+    seller_id INT NOT NULL,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    FOREIGN KEY (seller_id) REFERENCES users(id),
+    FOREIGN KEY (category_id) REFERENCES categories(id)
+)
+
