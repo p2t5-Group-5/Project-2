72359e0471da3edd984d0816e4918a2d5960a07f
Nukedeggz <sethmegg@yahoo.com>
2025-04-02 19:28:25 -0400

    pages Co-authored-by: Zawadi Brown <zawadi_sb@yahoo.com> Co-authored-by: Amcerna <Amcerna@users.noreply.github.com>

---
+++ client/src/pages/Contact.tsx
@@ -0,0 +1,6 @@
+import ContactForm from '../components/ContactForm';
+return(
+    <ContactForm/>
+);
+
+export default Contact;
---
+++ client/src/pages/Welcome.tsx
@@ -0,0 +1,37 @@
+import { useState, useEffect, useLayoutEffect } from "react";
+import { retrieveUsers } from "../api/userAPI";
+import type { UserData } from "../interfaces/UserData";
+
+const welcome = () =>{
+
+return(
+<>
+<header>
+<img src="" alt=""/> 
+<h1>Shop Name</h1>
+<button onClick={ go to cart!!!!!!!}> 
+
+</button>
+
+</header>
+<Navbar/>
+<main>
+
+<section className="flex">
+<div className="seller">
+<img src="" alt="" />
+<p></p>
+</div>
+<div className="item">
+<img src="" alt="" />
+<p></p>
+</div>
+</section>
+
+</main>
+<Footer/>  
+</>
+); 
+}
+
+export default welcome;
