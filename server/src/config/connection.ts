f66ce5171dbf38891daca29b7f24fcfd18c1ff92
Antonina S <antoninad22@gmail.com>
2025-04-02 12:24:33 -0500

    revert commit a3b3a2a

--- server/src/config/connection.ts
+++ server/src/config/connection.ts
@@ -3,17 +3,19 @@ dotenv.config();
 
 import { Sequelize } from 'sequelize';
 
-const sequelize = new Sequelize(
-      process.env.DB_NAME || '',
-      process.env.DB_USER || '',
-      process.env.DB_PASSWORD,
-      {
-        host: 'localhost',
-        dialect: 'postgres',
-        dialectOptions: {
-          decimalNumbers: true,
-        },
-      }
-    );
+const sequelize = process.env.DB_URL
+  ? new Sequelize(process.env.DB_URL)
+  : new Sequelize(
+    process.env.DB_NAME || '',
+    process.env.DB_USER || '',
+    process.env.DB_PASSWORD,
+    {
+      host: 'localhost',
+      dialect: 'postgres',
+      dialectOptions: {
+        decimalNumbers: true,
+      },
+    }
+  );
 
 export default sequelize;
