b2e5087c99b5a63da5f35014f9de42002aa75683
Zawadi Brown <zawadi_sb@yahoo.com>
2025-04-03 17:20:13 -0400

    started styling the components

--- client/src/components/Footer.tsx
+++ client/src/components/Footer.tsx
@@ -5,11 +5,13 @@ const Footer =() =>{
 
 return(
     <footer>
-        <Link to='/Contact'>Contact</Link><br/>
-        <Link to='/About'>About</Link><br/>
-        <a href='https://www.x.com'>twitter</a><br/>
-        <a href='https://www.youtube.com'>Youtube</a><br/>
-        <a href='https://wwww.Instaram.com'>Instagram</a><br/>
+        <div className='fflex'>
+            <Link to='/Contact'>Contact</Link><br/>
+            <Link to='/About'>About</Link><br/>
+            <a href='https://www.x.com'>Twitter</a><br/>
+            <a href='https://www.youtube.com'>Youtube</a><br/>
+            <a href='https://wwww.Instaram.com'>Instagram</a><br/>
+        </div>
         <p> ©️ T5P2</p>
     </footer>
 );
