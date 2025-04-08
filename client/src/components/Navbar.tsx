e4aa8cd7f609b0d6d94af5727f5fa8851279ab3a
Zawadi Brown <zawadi_sb@yahoo.com>
2025-04-07 22:48:09 -0400

    nav dropdown

--- client/src/components/Navbar.tsx
+++ client/src/components/Navbar.tsx
@@ -6,7 +6,6 @@ import "../styles/components.css";
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
-import NavDropdown from 'react-bootstrap/NavDropdown';
 
 const Navigation = () => {
   const [loginCheck, setLoginCheck] = useState(false);
